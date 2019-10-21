161b0a19559c676a1d7b442a47a02fc1791ef4ec
0x01 <zach.d.howard@gmail.com>
2019-10-21 14:13:20 -0700

    Update io_helpers.py

--- io_helpers.py
+++ io_helpers.py
@@ -1,9 +1,6 @@
 def checkdir(dir, new_folder):
-    ###
     ### Usage: checkdir('/mnt/drive/FOLDER', 'New Folder')
-    ###
     ### Checks the dir for a particular folder and if it does not exist, create it
-    ###
     folder = os.path.join(dir, new_folder)
     try:
         if not os.path.isdir(folder):
